48da19723279741024ed2a258c148ff5669ee43a
NHJ <98448432+skaguswjd0212@users.noreply.github.com>
2024-04-13 15:40:00 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -1,8 +1,7 @@
 star = int(input())
 
-for i in range(1, star):
+for i in range(1, star+1):
     print((star-i)*' '+'*'*i)
-print('*'*star)
 
-for i in range(star-1, 0, -1):
-    print((star-i)*' '+'*'*i)
+for j in range(1, star):
+    print(' '*(j)+(star-j)*'*')

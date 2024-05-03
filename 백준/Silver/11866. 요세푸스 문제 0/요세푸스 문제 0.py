73fc73fab64093f2e6aa59b20ba0e4630c70e219
NHJ <98448432+skaguswjd0212@users.noreply.github.com>
2024-05-03 22:53:27 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 92 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+removed_list = []
+queue = deque()
+
+#큐에 1~7 삽입
+for i in range(1, n+1):
+    queue.append(i)
+
+#큐가 빌 때까지
+while queue:
+    #k번째 값을 찾기 위해
+    #큐에서 k-1개의 값을 pop하고 맨 뒤에 append
+    for i in range(k-1):
+        queue.append(queue.popleft())
+    #결과 리스트에 k번째 값을 넣음
+    removed_list.append(queue.popleft())
+
+print("<", end='')
+for i in range(len(removed_list)):
+    print(removed_list[i], end='')
+    if i == len(removed_list)-1:
+        break
+    print(", ", end='')
+print(">")
